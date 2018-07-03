223fc2eb9c4e09b748a332889260bacd5760f9aa
Jorge T <jotolo7@gmail.com>
2018-07-03 12:04:22 -0400

    Configuring Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,4 +93,13 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation
 end
