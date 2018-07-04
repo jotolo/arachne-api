ef64cdf27a7632d9c3cbad0408bf083957e4ffa4
Jorge T <jotolo7@gmail.com>
2018-07-04 15:03:15 -0400

    ElasticSearch configuration, indexes and mapping for FTP file

--- app/models/ftp_file.rb
+++ app/models/ftp_file.rb
@@ -1,10 +1,41 @@
 class FtpFile < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   validates :name, presence: true
   validates :path, presence: true
 
   belongs_to :ftp_server
 
   before_save :set_address
+
+  settings index:
+               {
+                   number_of_shards: 1,
+                   analysis: { filter: { autocomplete_filter: { type: 'edge_ngram',
+                                                                min_gram: 1,
+                                                                max_gram: 20 } },
+                               analyzer: { autocomplete: { type: 'custom',
+                                                           tokenizer: 'standard',
+                                                           filter: %w(lowercase autocomplete_filter) }
+                               }
+                   }
+               } do
+    mappings dynamic: 'false' do
+      indexes :name, type: 'text', analyzer: 'autocomplete'
+      indexes :path, type: 'text', analyzer: 'autocomplete'
+      indexes :address, type: 'text', analyzer: 'autocomplete'
+    end
+  end
+
+  def self.search(query, results_count)
+    __elasticsearch__.search(from: 0, size: results_count,
+                             query: { multi_match: { query: query,
+                                                     analyzer: 'standard',
+                                                     fields: ['name^10', 'address^5', 'path'],
+                                                     type: 'cross_fields' } },
+                             sort: [{ name:  { order: 'asc' } }])
+  end
 
   private
   def set_address
